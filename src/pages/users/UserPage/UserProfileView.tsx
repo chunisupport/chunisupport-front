13c3e42c2a8801b39df4c0bc42ad113c183ea7d5
natua-tcmm <natua.tcmm@gmail.com>
2026-01-11 01:24:51 +0900

    feat: ユーザープロファイル表示機能を追加

--- src/pages/users/UserPage/UserProfileView.tsx
+++ src/pages/users/UserPage/UserProfileView.tsx
@@ -1,5 +1,5 @@
 import type { Component } from "solid-js";
-import type { UserProfileWithRecordsDTO } from "../../../types/api";
+import type { UserProfileWithRecordsDTO, PlayerDTO, PlayerRecordDTO, HonorDTO  } from "../../../types/api";
 
 type Props = {
   profile: UserProfileWithRecordsDTO;
@@ -7,11 +7,36 @@ type Props = {
 
 export const UserProfileView: Component<Props> = (props) => {
   const { profile } = props;
+  const playerInfo: PlayerDTO = profile.player;
+  const honors: HonorDTO[] = playerInfo.honors;
+
+  const bestRecords: PlayerRecordDTO[] = profile.records.best;
+  const newRecords: PlayerRecordDTO[] = profile.records.new;
+
+  const sumRating = (records: PlayerRecordDTO[]): number => {
+    return records.reduce((sum, record) => sum + record.rating, 0);
+  };
+
+  const bestRating = sumRating(bestRecords) / bestRecords.length;
+  const newRating = sumRating(bestRecords) / newRecords.length;
+  const playerRating = (sumRating(bestRecords)+sumRating(newRecords)) / (bestRecords.length + newRecords.length);
 
   return (
-    <div>
-      <h2>プロファイル取得成功</h2>
-      <pre>{JSON.stringify(profile, null, 2)}</pre>
+    <div class="m-4">
+      {/* ネームプレート */}
+      <div class="px-3 py-3 border border-gray-200 shadow-sm rounded-md ">
+        {/* <p class="mb-1 text-gray-400 text-xs">Chunisupport v1.0.0</p> */}
+        {/* TODO: 称号の背景画像を表示 */}
+        <p class="mb-3 p-1 bg-yellow-200 rounded-md text-sm text-center">{honors[0].name}</p>
+        <div class="mb-2 flex flex-row items-end justify-between">
+          <p class="">Lv. {playerInfo.level}</p>
+          <h1 class="flex-1 text-xl font-medium text-center">{playerInfo.name}</h1>
+        </div>
+        <hr class="mb-2 border-t border-gray-200" />
+        <p>Rating {playerRating.toFixed(4)} <span class="text-gray-500">(Best {bestRating.toFixed(4)} / New {newRating.toFixed(4)})</span></p>
+        {/* TODO: OverPowerのゲージみたいなのあるといいよね */}
+        <p>OP {playerInfo.overpower_percent}%</p>
+      </div>
     </div>
   );
 };
