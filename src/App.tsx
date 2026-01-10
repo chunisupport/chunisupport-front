8ae56f3254ce46f33b507489e2a2de9e0bdd0d7d
natua-tcmm <natua.tcmm@gmail.com>
2026-01-10 14:09:07 +0900

    chore: TailwindCSSを有効化

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import './App.css';
-
 const App = () => {
   return (
     <div class="content">
       <h1>Rsbuild with Solid</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <p class="border border-gray-500">Start building amazing things with Rsbuild.</p>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import './styles/tailwind.css';
 import { render } from 'solid-js/web';
 import App from './App';
 
