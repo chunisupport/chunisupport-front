d2037106cb46d61b612b1fe34a23bf306482e86c
natua-tcmm <natua.tcmm@gmail.com>
2026-01-10 22:41:01 +0900

    feat: ユーザーページにページタイトルを設定

--- src/pages/users/UserPage/UserPage.tsx
+++ src/pages/users/UserPage/UserPage.tsx
@@ -1,9 +1,13 @@
 import { useParams } from "@solidjs/router";
+import { Title } from "@solidjs/meta";
 
 const UserPage = () => {
     const params = useParams<{ username: string }>();
     return (
-        <h1>User Page: {params.username}</h1>
+        <>
+            <Title>{params.username}さんのページ - Chunisupport</Title>
+            <h1>User Page: {params.username}</h1>
+        </>
     );
 };
 
