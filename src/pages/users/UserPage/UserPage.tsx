7d1d041433bbda41501be40bf08f1d46cb98679a
natua-tcmm <natua.tcmm@gmail.com>
2026-01-13 03:00:18 +0900

    refactor: コードの整形とインポート順序の整理

--- src/pages/users/UserPage/UserPage.tsx
+++ src/pages/users/UserPage/UserPage.tsx
@@ -1,37 +1,31 @@
-import { Suspense, ErrorBoundary, Show } from "solid-js";
-import type { Component } from "solid-js";
-import { useParams } from "@solidjs/router";
 import { Title } from "@solidjs/meta";
-import { createResource } from "solid-js";
+import { useParams } from "@solidjs/router";
+import type { Component } from "solid-js";
+import { createResource, ErrorBoundary, Show, Suspense } from "solid-js";
 
 import { fetchUserProfile } from "../../../api/users";
-import { UserProfileView } from "./UserProfileView";
 import { Loading } from "../../../components";
+import { UserProfileView } from "./UserProfileView";
 
 const UserPage: Component = () => {
-  const params = useParams<{ username: string }>();
+	const params = useParams<{ username: string }>();
 
-  const [userProfile] = createResource(
-    () => params.username,
-    fetchUserProfile,
-  );
+	const [userProfile] = createResource(() => params.username, fetchUserProfile);
 
-  return (
-    <>
-      <Title>{params.username}さんのページ - Chunisupport</Title>
-      <Suspense fallback={<Loading />}>
-        <ErrorBoundary
-          fallback={(err) => (
-            <p class="text-red-500">ERROR: {err.message}</p>
-          )}
-        >
-          <Show when={userProfile()}>
-            {(profile) => <UserProfileView profile={profile()} />}
-          </Show>
-        </ErrorBoundary>
-      </Suspense>
-    </>
-  );
+	return (
+		<>
+			<Title>{params.username}さんのページ - Chunisupport</Title>
+			<Suspense fallback={<Loading />}>
+				<ErrorBoundary
+					fallback={(err) => <p class="text-red-500">ERROR: {err.message}</p>}
+				>
+					<Show when={userProfile()}>
+						{(profile) => <UserProfileView profile={profile()} />}
+					</Show>
+				</ErrorBoundary>
+			</Suspense>
+		</>
+	);
 };
 
 export default UserPage;
