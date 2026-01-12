4bf33d565f5dd287d5513bd0ac106933eb3b77a9
natua-tcmm <natua.tcmm@gmail.com>
2026-01-13 03:40:48 +0900

    feat: ユーザープロファイルにタブ切り替え機能と画像化ボタンを追加

--- src/pages/users/UserPage/UserProfileView.tsx
+++ src/pages/users/UserPage/UserProfileView.tsx
@@ -1,3 +1,5 @@
+import * as Tabs from "@kobalte/core/tabs";
+import { Image } from "lucide-solid";
 import type { Component } from "solid-js";
 import type {
 	HonorDTO,
@@ -46,9 +48,9 @@ export const UserProfileView: Component<Props> = (props) => {
 	};
 
 	return (
-		<div class="m-4">
+		<div class="my-4">
 			{/* ネームプレート */}
-			<div class="px-3 py-3 border border-gray-200 shadow-sm rounded-md ">
+			<div class="mb-4 mx-4 px-3 py-3 border border-gray-200 shadow-sm rounded-md ">
 				{/* <p class="mb-1 text-gray-400 text-xs">Chunisupport v1.0.0</p> */}
 				{/* TODO: 称号の背景画像を表示 */}
 				<p class="mb-3 p-1 bg-yellow-200 rounded-md text-sm text-center">
@@ -71,30 +73,76 @@ export const UserProfileView: Component<Props> = (props) => {
 				<p>OP {playerInfo.overpower_percent}%</p>
 			</div>
 
-			{/* TODO: タブ切り替え */}
-			<div class="mb-4"></div>
-
-			{/* BEST枠一覧 */}
-			<div class="">
-				{bestRecords.map((record, i) => (
-					<div
-						class={`mb-2 p-3 rounded-md border ${getDifficultyColors(record.difficulty)}`}
+			{/* タブ切り替え */}
+			<Tabs.Root defaultValue="best" class="mb-4">
+				<Tabs.List class="flex gap-2 mb-4 px-4 border-b border-gray-300">
+					<Tabs.Trigger
+						value="best"
+						class="px-3 py-1 rounded-t data-selected:bg-white data-selected:border-b-2 data-selected:border-blue-500"
 					>
-						<div class="flex gap-3">
-							<div class="flex flex-col">
-								<p># {i + 1}</p>
-								<p class="text-sm">{record.rating.toFixed(2)}</p>
+						ベスト枠
+					</Tabs.Trigger>
+					<Tabs.Trigger
+						value="new"
+						class="px-3 py-1 rounded-t data-selected:bg-white data-selected:border-b-2 data-selected:border-blue-500"
+					>
+						新曲枠
+					</Tabs.Trigger>
+					<div class="flex-1"></div>
+					{/* 画像化ボタン */}
+					<button
+						type="button"
+						class="px-3 py-1 mb-1 bg-blue-500 text-white rounded-md"
+					>
+						<Image class="inline-block mr-1 mb-0.5" size={16} />
+						画像化
+					</button>
+				</Tabs.List>
+				<Tabs.Content value="best">
+					<div class="mx-4">
+						{bestRecords.map((record, i) => (
+							<div
+								class={`mb-2 p-3 rounded-md border ${getDifficultyColors(record.difficulty)}`}
+							>
+								<div class="flex gap-3">
+									<div class="flex flex-col">
+										<p># {i + 1}</p>
+										<p class="text-sm">{record.rating.toFixed(2)}</p>
+									</div>
+									<div>
+										<p class="font-medium">{record.title}</p>
+										<p class="text-sm">
+											{record.const} / {record.score}
+										</p>
+									</div>
+								</div>
 							</div>
-							<div>
-								<p class="font-medium">{record.title}</p>
-								<p class="text-sm">
-									{record.const} / {record.score}
-								</p>
-							</div>
-						</div>
+						))}
 					</div>
-				))}
-			</div>
+				</Tabs.Content>
+				<Tabs.Content value="new">
+					<div class="mx-4">
+						{newRecords.map((record, i) => (
+							<div
+								class={`mb-2 p-3 rounded-md border ${getDifficultyColors(record.difficulty)}`}
+							>
+								<div class="flex gap-3">
+									<div class="flex flex-col">
+										<p># {i + 1}</p>
+										<p class="text-sm">{record.rating.toFixed(2)}</p>
+									</div>
+									<div>
+										<p class="font-medium">{record.title}</p>
+										<p class="text-sm">
+											{record.const} / {record.score}
+										</p>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				</Tabs.Content>
+			</Tabs.Root>
 		</div>
 	);
 };
