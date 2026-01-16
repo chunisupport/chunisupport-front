bbba881b2374b5e2ab6b15cfb90f3e292ffabf47
natua-tcmm <natua.tcmm@gmail.com>
2026-01-16 21:18:26 +0900

    feat: スクロール量とアニメーションを調整

--- src/pages/users/UserPage/components/UserRecordCard.tsx
+++ src/pages/users/UserPage/components/UserRecordCard.tsx
@@ -30,6 +30,12 @@ export const UserRecordCard: Component<Props> = (props) => {
 
 	onMount(() => {
 		if (titleRef && titleRef.scrollWidth > titleRef.clientWidth) {
+			// はみ出している割合を計算
+			const overflowPercentage =
+				((titleRef.scrollWidth - titleRef.clientWidth) / titleRef.clientWidth) *
+				100;
+			// CSS変数に設定
+			titleRef.style.setProperty("--scroll-amount", `-${overflowPercentage}%`);
 			setShouldAnimate(true);
 		}
 	});
