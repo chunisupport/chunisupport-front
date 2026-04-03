013972d076c680acb8e0a3b753245566192e76dc
キューマン・エノビクト <30486982+Qman11010101@users.noreply.github.com>
2026-04-03 23:59:12 +0900

    feat: Firebase設定を環境変数から取得するように変更

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,13 @@
 
 interface ImportMetaEnv {
   readonly PUBLIC_BACKEND_URL: string
+  readonly FB_API_KEY: string
+  readonly FB_AUTH_DOMAIN: string
+  readonly FB_PROJECT_ID: string
+  readonly FB_STORAGE_BUCKET: string
+  readonly FB_MESSAGING_SENDER_ID: string
+  readonly FB_APP_ID: string
+  readonly FB_MEASUREMENT_ID: string
 }
 
 interface ImportMeta {
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { GoogleAuthProvider, getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDyXaZoiz5yQjSM5ff1-X1-QoGGm5tYJmE',
-  authDomain: 'chunisupport-develop.firebaseapp.com',
-  projectId: 'chunisupport-develop',
-  storageBucket: 'chunisupport-develop.firebasestorage.app',
-  messagingSenderId: '269920974279',
-  appId: '1:269920974279:web:e77fe0be1be7d4876ad5e2',
-  measurementId: 'G-V3GGF7S5F6',
+  apiKey: import.meta.env.FB_API_KEY,
+  authDomain: import.meta.env.FB_AUTH_DOMAIN,
+  projectId: import.meta.env.FB_PROJECT_ID,
+  storageBucket: import.meta.env.FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FB_APP_ID,
+  measurementId: import.meta.env.FB_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
