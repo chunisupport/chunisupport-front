f568dce302f0608a2ef3f940c574d0028592db88
natua-tcmm <natua.tcmm@gmail.com>
2026-07-04 23:50:30 +0900

    fix: refactorに伴うレコードテーブルの表示バグを修正

--- src/components/common/record/RecordDisplayParts.tsx
+++ src/components/common/record/RecordDisplayParts.tsx
@@ -51,9 +51,11 @@ export const RECORD_ROW_HOVER_WITH_TOP_BORDER_CLASS = `${RECORD_ROW_HOVER_CLASS}
 export const RECORD_CARD_HOVER_CLASS = `${RECORD_HOVER_TRANSITION_CLASS} group-hover:bg-interactive-row-hover`
 /** 仮想スクロールで使うレコード1行の高さ。 */
 export const RECORD_ROW_HEIGHT = 34
+const RECORD_ROW_MIN_HEIGHT_CLASS_BY_HEIGHT = {
+  [RECORD_ROW_HEIGHT]: 'min-h-[34px]',
+} as const satisfies Record<typeof RECORD_ROW_HEIGHT, `min-h-[${typeof RECORD_ROW_HEIGHT}px]`>
 /** レコード1行分の最小高さを揃えるクラス。 */
-export const RECORD_ROW_MIN_HEIGHT_CLASS =
-  `min-h-[${RECORD_ROW_HEIGHT}px]` as const satisfies `min-h-[${typeof RECORD_ROW_HEIGHT}px]`
+export const RECORD_ROW_MIN_HEIGHT_CLASS = RECORD_ROW_MIN_HEIGHT_CLASS_BY_HEIGHT[RECORD_ROW_HEIGHT]
 /** レコード表ヘッダーボタンの共通レイアウトクラス。 */
 export const RECORD_HEADER_BUTTON_CLASS = RECORD_ROW_MIN_HEIGHT_CLASS
 /** 数値や英数字中心のレコード列に使う文字サイズクラス。 */
